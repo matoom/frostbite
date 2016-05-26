500618838c8b61cbc27c8ad38027ba68a4b6ecc6
matoom <madtoom@gmail.com>
2016-05-26 15:29:13 +0300

    v1.4.0

--- gui/version.h
+++ gui/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,2,0,0
-#define VER_FILEVERSION_STR         "1.2.0.0\0"
+#define VER_FILEVERSION             1,4,0,0
+#define VER_FILEVERSION_STR         "1.4.0.0\0"
 
-#define VER_PRODUCTVERSION          1,2,0,0
-#define VER_PRODUCTVERSION_STR      "1.2\0"
+#define VER_PRODUCTVERSION          1,4,0,0
+#define VER_PRODUCTVERSION_STR      "1.4\0"
 
 #define VER_COMPANYNAME_STR         "matoom"
 #define VER_FILEDESCRIPTION_STR     "DragonRealms MUD Frontend"
