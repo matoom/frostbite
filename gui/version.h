b59b25cf927463e635eaff6bc5af065984d30d36
matoom <madtoom@gmail.com>
2017-07-13 17:13:11 +0300

    v1.6.5b

--- gui/version.h
+++ gui/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,4,0,0
-#define VER_FILEVERSION_STR         "1.4.0.0\0"
+#define VER_FILEVERSION             1,6,5,0
+#define VER_FILEVERSION_STR         "1.6.5.0\0"
 
-#define VER_PRODUCTVERSION          1,4,0,0
-#define VER_PRODUCTVERSION_STR      "1.4\0"
+#define VER_PRODUCTVERSION          1,6,5,0
+#define VER_PRODUCTVERSION_STR      "1.6\0"
 
 #define VER_COMPANYNAME_STR         "matoom"
 #define VER_FILEDESCRIPTION_STR     "Frostbite Frontend"
