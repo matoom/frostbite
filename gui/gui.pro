0f32e353de6ac2adb9b8ab30d62056e3e1a00198
matoom <madtoom@gmail.com>
2018-10-27 19:41:18 +0300

    v1.9.0b

--- gui/gui.pro
+++ gui/gui.pro
@@ -4,7 +4,7 @@
 #
 #-------------------------------------------------
 
-RELEASE_VERSION = 1.8.1b
+RELEASE_VERSION = 1.9.0b
 
 DEFINES += RELEASE_VERSION=\\\"$$RELEASE_VERSION\\\"
 
--- gui/version.h
+++ gui/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,6,5,0
-#define VER_FILEVERSION_STR         "1.6.5.0\0"
+#define VER_FILEVERSION             1,9,0,0
+#define VER_FILEVERSION_STR         "1.9.0b"
 
-#define VER_PRODUCTVERSION          1,6,5,0
-#define VER_PRODUCTVERSION_STR      "1.6\0"
+#define VER_PRODUCTVERSION          1,9,0,0
+#define VER_PRODUCTVERSION_STR      "1.9.0b"
 
 #define VER_COMPANYNAME_STR         "matoom"
 #define VER_FILEDESCRIPTION_STR     "Frostbite Frontend"
