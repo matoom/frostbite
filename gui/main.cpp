301caed3d6be5a55fa10be09ff772f24cc3a2ee8
matoom <madtoom@gmail.com>
2019-08-21 21:46:03 +0300

    enable high dpi scaling

--- gui/main.cpp
+++ gui/main.cpp
@@ -33,6 +33,15 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setApplicationVersion(QString(RELEASE_VERSION));
     QCoreApplication::setApplicationName("Frostbite");
 
+    static const char ENV_VAR_QT_DEVICE_PIXEL_RATIO[] = "QT_DEVICE_PIXEL_RATIO";
+    if (!qEnvironmentVariableIsSet(ENV_VAR_QT_DEVICE_PIXEL_RATIO)
+            && !qEnvironmentVariableIsSet("QT_AUTO_SCREEN_SCALE_FACTOR")
+            && !qEnvironmentVariableIsSet("QT_SCALE_FACTOR")
+            && !qEnvironmentVariableIsSet("QT_SCREEN_SCALE_FACTORS")) {
+        QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+        QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    }
+
     #ifdef __linux__
     signal(SIGSEGV, handler);
     #endif
