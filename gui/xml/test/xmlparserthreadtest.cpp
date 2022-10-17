ebc630ef33d971f26fb5f4aa60bafb84b889082c
Alexey Veretennikov <fourier@protonmail.ch>
2022-10-17 21:50:50 +0200

    Added 2 testcases, one failed test for issue #117

--- gui/xml/test/xmlparserthreadtest.cpp
+++ gui/xml/test/xmlparserthreadtest.cpp
@@ -1,25 +1,76 @@
-#include "xml/xmlparserthread.h"
 #include <QtTest/QtTest>
+#include <map>
+#include "xml/xmlparserthread.h"
+#include "hyperlinkutils.h"
+
+class GameTextCollector : public QObject {
+    Q_OBJECT
+public:
+    GameTextCollector(XmlParserThread* xmlParser) : QObject(xmlParser) {
+        connect(xmlParser, SIGNAL(writeText(QByteArray, bool)), this,
+                SLOT(writeGameText(QByteArray, bool)));
+    }
+    QString text;
+    bool prompt;
+public slots:
+    void writeGameText(QByteArray text, bool prompt) {
+        this->text = text;
+        this->prompt = prompt;
+    }
+};
 
 class XmlParserThreadTest : public QObject {
     Q_OBJECT
+public:
 private slots:
 
-    void toUpper() {
-        XmlParserThread* xmlparser = new XmlParserThread(this, NULL);
-        
-        connect(xmlparser, SIGNAL(writeText(QByteArray, bool)), this,
-                SLOT(writeGameText(QByteArray, bool)));
-        
-        xmlparser->onProcess("<pushBold/>******************************************************\
+    void pushBoldTestCase() {
+        XmlParserThread* xmlParser = new XmlParserThread(this, NULL);
+        GameTextCollector* textCollector = new GameTextCollector(xmlParser);
+
+        static const QString input =
+                "<pushBold/>******************************************************\
 *** Please check <d>NEWS NEXT</d> for unread <d>NEWS</d> items. ***\
 ******************************************************\
 <popBold/><prompt time=\"1665949176\">s&gt;</prompt>\
-");
-        QCOMPARE(10, 10);
+";
+        static const QString expect =
+                "<span class=\"bold\">********************************************************* "
+                "Please check </span><a href=\"f://a/TkVXUyBORVhU\">NEWS NEXT</a><span "
+                "class=\"bold\"> for unread </span><a href=\"f://a/TkVXUw==\">NEWS</a><span "
+                "class=\"bold\"> items. "
+                "*********************************************************</"
+                "span>********************************************************* Please check NEWS "
+                "NEXT for unread NEWS items. "
+                "*********************************************************s>";
+        xmlParser->onProcess(input.toLocal8Bit());
+        QCOMPARE(textCollector->text, expect);
+
+        delete xmlParser;
     }
 
-    void writeGameText(QByteArray text, bool prompt) { qDebug() << text; }
+    void d_cmd_TestCase() {
+        XmlParserThread* xmlParser = new XmlParserThread(this, NULL);
+        GameTextCollector* textCollector = new GameTextCollector(xmlParser);
+
+        // a list of pairs: input data, expected result
+        // clang-format off
+        static std::pair<QString, QString> testData [] = {
+            {"  <d cmd='inventory'>INV</d>               - Shows a description of all of the items you are currently wearing",
+             "<a href=\"f://a/aW52ZW50b3J5\">INV</a>               - Shows a description of all of the items you are currently wearing"},
+            {"<d cmd='inv slots'>INV SLOTS</d>         - Gives a summary of your inventory slots, showing amount worn in each.",
+             "<a href=\"f://a/aW52IHNsb3Rz\">INV SLOTS</a>         - Gives a summary of your inventory slots, showing amount worn in each."},
+            {" INV [<d cmd='inv armor'>ARMOR</d> | <d cmd='inv weapon'>WEAPON</d> | <d cmd='inv fluff'>FLUFF</d> | <d cmd='inv container'>CONTAINER</d> | <d cmd='inv combat'>COMBAT</d>] - Shows you the items of this type that you're wearing.",
+             "INV [<a href=\"f://a/aW52IGFybW9y\">ARMOR</a> | <a href=\"f://a/aW52IHdlYXBvbg==\">WEAPON</a> | <a href=\"f://a/aW52IGZsdWZm\">FLUFF</a> | <a href=\"f://a/aW52IGNvbnRhaW5lcg==\">CONTAINER</a> | <a href=\"f://a/aW52IGNvbWJhdA==\">COMBAT</a>] - Shows you the items of this type that you're wearing."}
+        };
+        // clang-format on
+        for (size_t i = 0; i < sizeof(testData) / sizeof(testData[0]); ++i) {
+            xmlParser->onProcess(testData[i].first.toLocal8Bit());
+            qDebug() << textCollector->text;
+            QCOMPARE(textCollector->text, testData[i].second);
+        }
+        delete xmlParser;
+    }
 };
 
 QTEST_MAIN(XmlParserThreadTest)
